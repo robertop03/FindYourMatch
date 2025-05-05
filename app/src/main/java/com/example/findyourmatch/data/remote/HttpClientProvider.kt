552e91d2533852255002ee1094e84e2fdd9b198d
Roberto Pisu <roberto.pisu03@gmail.com>
2025-05-06 00:22:44 +0200

    Implementate api per il caricamento dei paesi dell'unione europea e le relative province nella creazione dell'utente

---
+++ app/src/main/java/com/example/findyourmatch/data/remote/HttpClientProvider.kt
@@ -0,0 +1,15 @@
+package com.example.findyourmatch.data.remote
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+fun createHttpClient(): HttpClient {
+    return HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+    }
+}
