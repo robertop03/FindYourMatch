2c4ab47f4cd053d0efad4d5c064341a29d2dba73
Marco Giugliani <marco.giugliani@studio.unibo.it>
2025-05-31 17:12:38 +0200

    Cominciata roba su foto profilo

--- app/src/main/java/com/example/findyourmatch/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/findyourmatch/viewmodel/ProfileViewModel.kt
@@ -1,27 +1,41 @@
 package com.example.findyourmatch.viewmodel
 
 import android.app.Application
+import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.application
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.findyourmatch.data.user.AnagraficaUtente
 import com.example.findyourmatch.data.user.IndirizzoUtente
 import com.example.findyourmatch.data.user.getIndirizzoUtente
 import com.example.findyourmatch.data.user.getUserInfo
+import com.example.findyourmatch.data.user.updateProfileImage
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(application: Application) : AndroidViewModel(application) {
     private val _user = MutableStateFlow<AnagraficaUtente?>(null)
     private val _userAddress = MutableStateFlow<IndirizzoUtente?>(null)
+    private val _profileImageUri = MutableStateFlow<Uri?>(null)
     val user = _user
     val userAddress = _userAddress
+    val profileImageUri: StateFlow<Uri?> = _profileImageUri
 
     init {
         viewModelScope.launch {
             _user.value = getUserInfo(application)
             _userAddress.value = getIndirizzoUtente(application)
+        }
+    }
+
+    fun saveLocalProfileImageUri(uri: Uri) {
+        _profileImageUri.value = uri
+        viewModelScope.launch {
+            updateProfileImage(application, _user.value!!.email, _profileImageUri.value!!)
         }
     }
 }
