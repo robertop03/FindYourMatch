95498c02c2ad31460dd525bd5bb47cf31b1aa55a
Marco Giugliani <marco.giugliani@studio.unibo.it>
2025-06-10 15:48:36 +0200

    Quasi completato tutto, problema nell'aggiornamento dei raggiungimenti delle medaglie

--- app/src/main/java/com/example/findyourmatch/data/notifications/NotificationsHandler.kt
+++ app/src/main/java/com/example/findyourmatch/data/notifications/NotificationsHandler.kt
@@ -394,6 +394,49 @@ suspend fun aggiungiNotificaAccettazione(
     }
 }
 
+suspend fun aggiungiNotificaRecensione(
+    context: Context,
+    titolo: String,
+    testo: String,
+    destinatario: String,
+    tipologia: String = "recensione",
+    titoloEn: String,
+    testoEn: String,
+    idPartita: Int,
+    destinatarioRecensione: String,
+    autoreRecensione: String
+): Boolean = withContext(Dispatchers.IO) {
+    val client = OkHttpClient()
+    val token = SessionManager.getAccessToken(context) ?: return@withContext false
+
+    val jsonBody = """
+        {
+            "titolo": "$titolo",
+            "testo": "$testo",
+            "destinatario": "$destinatario",
+            "tipologia": "$tipologia",
+            "titolo_en": "$titoloEn",
+            "testo_en": "$testoEn",
+            "partita":"$idPartita",
+            "destinatario_recensione": "$destinatarioRecensione",
+            "autore_recensione": "$autoreRecensione"
+        }
+    """.trimIndent()
+    val requestBody = jsonBody.toRequestBody("application/json".toMediaTypeOrNull())
+
+    val request = Request.Builder()
+        .url("https://ugtxgylfzblkvudpnagi.supabase.co/rest/v1/notifiche")
+        .post(requestBody)
+        .addHeader("apikey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVndHhneWxmemJsa3Z1ZHBuYWdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4ODI4NTUsImV4cCI6MjA2MjQ1ODg1NX0.cc0z6qkcWktvnh83Um4imlCBSfPlh7TelMNFIhxmjm0")
+        .addHeader("Authorization", "Bearer $token")
+        .addHeader("Content-Type", "application/json")
+        .build()
+
+    client.newCall(request).execute().use { response ->
+        return@withContext response.isSuccessful
+    }
+}
+
 suspend fun segnaNotificaComeGestita(context: Context, idNotifica: Int): Boolean = withContext(Dispatchers.IO) {
     val client = OkHttpClient()
     val token = SessionManager.getAccessToken(context) ?: return@withContext false
